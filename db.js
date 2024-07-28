28b0d7e8c727920fded533846e0a2a267561fd02
imrancse190 <imrancse190@gmail.com>
2024-07-29 00:32:35 +0600

    "Connect with git"

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//   user: process.env.USER,
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORTDB,
-// });
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "hotel_management",
-  password: "p@stgress",
-  port: "5433",
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORTDB,
 });
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "hotel_management",
+//   password: "p@stgress",
+//   port: "5433",
+// });
 
 module.exports = pool;
